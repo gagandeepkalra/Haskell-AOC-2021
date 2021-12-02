96cae944f524bb3c85768ff43cc7bfe85257d7c4
Gagandeep Kalra <gagandeepkalra1994@gmail.com>
2021-12-02 21:10:21 +0800

    solved day 01

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,25 @@
 module Main where
 
-import Lib
+import Data.Foldable
+import qualified Data.Map as Map
+import qualified Day01 (solve)
+import System.Environment
+import System.Exit
+import Control.Monad
+
+solutions = Map.fromList [("01", Day01.solve)]
+
+solveSingle :: String -> IO ()
+solveSingle s = case Map.lookup s solutions of
+  Just f -> readFile (concat ["./data/day", s, ".txt"]) >>= f
+  Nothing -> putStrLn $ "Day not implemented: " ++ s
+
+solveProblems :: [String] -> IO ()
+solveProblems = mapM_ solveSingle
 
 main :: IO ()
-main = someFunc
+main = do
+  args <- getArgs
+  if null args
+    then solveProblems $ Map.keys solutions
+    else solveProblems args
---
+++ src/Day01.hs
@@ -0,0 +1,21 @@
+module Day01 (solve) where
+  
+part1:: [Int] -> Int
+part1 = 
+  let
+    go:: (Int, Maybe Int) -> Int -> (Int, Maybe Int)
+    go (acc, Just prev) curr = (if prev < curr then acc + 1 else acc, Just curr)
+    go (acc, Nothing) curr = (acc, Just curr)
+  in fst . foldl go (0, Nothing) 
+  
+part2:: [Int] -> Int
+part2 =
+  let 
+    go:: Int -> [Int] -> Int
+    go acc (a:b:c:d:xs) = go (if a < d then acc + 1 else acc) (b:c:d:xs) 
+    go acc _ = acc
+   in go 0
+   
+solve:: String -> IO ()
+solve input = putStrLn "--- Day 01 ---" >> print (part1 $ p input) >> print (part2 $ p input)
+    where p = map read . lines
